cab177f637add3cc0999155dfc95aaca449231db
Park-Junho1 <june981021@gmail.com>
2024-11-18 10:56:29 +0900

    level_1 완료

--- src/main/java/level_1/Main.java
+++ src/main/java/level_1/Main.java
@@ -1,7 +1,31 @@
 package level_1;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String s = st.nextToken();
+            if (s.startsWith("e")) {
+                break;
+            }
+            int Num1 = Integer.parseInt(s);
+            char operator = st.nextToken().charAt(0);
+            int Num2 = Integer.parseInt(st.nextToken());
+            if (operator == '+') {
+                result = Num1 + Num2;
+            } else if (operator == '-') {
+                result = Num1 - Num2;
+            } else if (operator == '*') {
+                result = Num1 * Num2;
+            } else if (operator == '/') {
+                result = Num1 / Num2;
+            }
+            System.out.println(result);
+        }
+        br.close();
     }
 }
