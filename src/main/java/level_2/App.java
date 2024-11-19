b2f94ef97bf72c33c9b49313270b34db43508990
Park-Junho1 <june981021@gmail.com>
2024-11-19 10:32:23 +0900

    level_2 완료

--- src/main/java/level_2/App.java
+++ src/main/java/level_2/App.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws IOException {
         Calculator calculator = new Calculator();
-        Queue<Integer> arr = new LinkedList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while(true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -13,8 +12,17 @@ public class App {
             if (s.startsWith("e")) {
                 break;
             } else if (s.startsWith("p")) {
-                calculator.removeResult(arr);
+                calculator.removeResult();
                 continue;
+            } else if (s.startsWith("i")) {
+                int k = Integer.parseInt(st.nextToken());
+                int index_value = calculator.ListRetrieve(k);
+                System.out.println(index_value);
+                continue;
+            } else if (s.startsWith("c")) {
+                int k = Integer.parseInt(st.nextToken());
+                int value = Integer.parseInt(st.nextToken());
+                calculator.ListModify(k, value);
             }
             int Num1 = Integer.parseInt(s);
             char Operator = st.nextToken().charAt(0);
@@ -25,7 +33,6 @@ public class App {
             calculator.SetOperator(Operator);
 
             int result = calculator.calculate();
-            arr.add(result);
             System.out.println(result);
         }
         br.close();
--- src/main/java/level_2/Calculator.java
+++ src/main/java/level_2/Calculator.java
@@ -5,6 +5,8 @@ public class Calculator {
     private int Num1;
     private int Num2;
     private char Operator;
+    private int result;
+    private Queue<Integer> arr = new LinkedList<>();
 
     public int GetNum1() {
         return Num1;
@@ -32,21 +34,37 @@ public class Calculator {
 
     public int calculate () {
         switch (Operator) {
-            case '+' : return Num1 + Num2;
-            case '-' : return Num1 - Num2;
-            case '*' : return Num1 * Num2;
-            case '/' : return (Num2 != 0) ? Num1 / Num2 : 0;
+            case '+' : result = Num1 + Num2; break;
+            case '-' : result = Num1 - Num2; break;
+            case '*' : result = Num1 * Num2; break;
+            case '/' : result = (Num2 != 0) ? Num1 / Num2 : 0; break;
             default : throw new IllegalArgumentException("Operator not recognised");
         }
+        arr.add(result);
+        return result;
+    }
+    public void removeResult () {
+        arr.poll();
     }
 
-    public void removeResult (Queue<Integer> arr) {
-        arr.poll();
+    private List<Integer> list = new LinkedList<>(arr);
+
+    public int ListRetrieve (int k) {
+        int value = 0;
+        if (k >= 0 && k < list.size()) {
+            value = list.get(k);
+        }
+        return value;
+    }
+
+    public void ListModify (int k, int value) {
+        list.set(k, value);
     }
 }
 
 /*처음에 클래스 생성자를 통해서 연결시키려고 했더니 Getter과 Setter를 사용하여 연결하란다.
-* 생성자를 통한 연결이 간접적이지 않아서 그런가 했는데, 그건 또 아닌 모양이네.*/
+* 생성자를 통한 연결이 간접적이지 않아서 그런가 했는데, 그건 또 아닌 모양이네.
+* 객체 자체를 초기화하냐(클래스 생성자 이용), 값 하나하나를 받아서 객체를 재활용하냐(get, set)*/
 /* Queue나 Stack 같은 걸 넘길때는 Queue<Integer>같은 식으로 넘긴다.*/
 /*또한, Getter와 Setter 역할에 대하여 이 코드에서 단순 조회가 아닌 가져오는 역할까지 Setter가 한다.
 * Getter의 역할은 단지, 이 클래스에게서부터 어딘가가 값을 요구할 경우를 대비한 메서드에 불과하다.*/
